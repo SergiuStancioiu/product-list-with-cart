03eef85f031eadc1a30c6459747259cf14e46c00
Sergiu Stancioiu <s.sergiu96@gmail.com>
2024-08-10 12:15:11 +0300

    feat: started working on cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <!--Cart-->
+  <div class="cart-container">
+    <div class="cart-heading">
+      <h2>Your Cart (0)</h2>
+    </div>
+    <div class="cart-content">
+      <div class="cart-empty-image">
+        <img
+          src="../assets/images/illustration-empty-cart.svg"
+          alt="Empty Cart"
+        />
+      </div>
+      <div class="cart-info">
+        <p>Your added items will appear here</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
